c77af330fa745240aa5fd7927979b78094a911fe
Moayadhamdan <hamadanjo@gmail.com>
2024-05-12 20:57:53 +0000

    lab 19

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home/Home.js";
-import MovieList from "./components/MovieList/MovieList.js";
+import FavList from "./components/FavList/FavList.js";
+import Navigation from "./components/Navbar/Navbar.js";
 
 
 function App() {
   return (
     <>
+    <Navigation/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/favMovieList" element={<MovieList />}></Route>
+        <Route path="/favMovieList" element={<FavList />}></Route>
       </Routes>
     </>
   );
 }
 
 export default App;
+
+
+
+
+
+
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,79 +1,91 @@
-import React, { useState } from 'react';
-import ModalMovie from '../ModalMovie/ModalMovie.js';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import './Movie.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import ModalMovie from "../ModalMovie/ModalMovie.js";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import "./Movie.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDoubleDown,
+  faAngleDoubleUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Movie(props) {
-    const [showModal, setShowModal] = useState(false);
-    const [selectedMovie, setSelectedMovie] = useState(null);
-    const [comment, setComment] = useState('');
-    const [showFullOverview, setShowFullOverview] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [comment, setComment] = useState("");
+  const [showFullOverview, setShowFullOverview] = useState({});
 
-    const handleModalClose = () => {
-        setShowModal(false);
-        setSelectedMovie(null);
-        setComment('');
-    };
+  const handleModalClose = () => {
+    setShowModal(false);
+    setSelectedMovie(null);
+    setComment("");
+  };
 
-    const handleAddToFavorites = (movie) => {
-        setSelectedMovie(movie);
-        setShowModal(true);
-    };
+  const handleAddToFavorites = (movie) => {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  };
 
-    const toggleOverview = (id) => {
-        setShowFullOverview({
-            ...showFullOverview,
-            [id]: !showFullOverview[id]
-        });
-    };
+  const toggleOverview = (id) => {
+    setShowFullOverview({
+      ...showFullOverview,
+      [id]: !showFullOverview[id],
+    });
+  };
 
-    const renderOverview = (id) => {
-        if (showFullOverview[id]) {
-            return props.overview;
-        } else {
-            const words = props.overview.split(' ');
-            const firstSixWords = words.slice(0, 6).join(' ');
-            return `${firstSixWords}...`;
-        }
-    };
+  const renderOverview = (id) => {
+    if (showFullOverview[id]) {
+      return props.overview;
+    } else {
+      const words = props.overview.split(" ");
+      const firstSixWords = words.slice(0, 6).join(" ");
+      return `${firstSixWords}...`;
+    }
+  };
 
-    return (
-        <>
-            <Col key={props.id} className="movie-col">
-                <Card className="movie-card">
-                    <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${props.image}`} className="card-image" />
-                    <Card.Body>
-                        <Card.Title>{props.title}</Card.Title>
-                        <Card.Text className="card-text">
-                            {renderOverview(props.id)}
-                            {!showFullOverview[props.id] && (
-                                <Button variant="link" onClick={() => toggleOverview(props.id)}>
-                                    <FontAwesomeIcon icon={faAngleDoubleDown} /> See More
-                                </Button>
-                            )}
-                            {showFullOverview[props.id] && (
-                                <Button variant="link" onClick={() => toggleOverview(props.id)}>
-                                    <FontAwesomeIcon icon={faAngleDoubleUp} /> See Less
-                                </Button>
-                            )}
-                        </Card.Text>
-                        <Button variant="primary" onClick={() => handleAddToFavorites(props)}>Add to Favorites</Button>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <ModalMovie
-                showModal={showModal}
-                handleModalClose={handleModalClose}
-                selectedMovie={selectedMovie}
-                comment={comment}
-                setComment={setComment}
-            />
-        </>
-    );
+  return (
+    <>
+      <Col key={props.id} className="movie-col">
+        <Card className="movie-card">
+          <Card.Img
+            variant="top"
+            src={`https://image.tmdb.org/t/p/w500${props.image}`}
+            className="card-image"
+          />
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Text className="card-text">
+              {renderOverview(props.id)}
+              {!showFullOverview[props.id] && (
+                <Button variant="link" onClick={() => toggleOverview(props.id)}>
+                  <FontAwesomeIcon icon={faAngleDoubleDown} /> See More
+                </Button>
+              )}
+              {showFullOverview[props.id] && (
+                <Button variant="link" onClick={() => toggleOverview(props.id)}>
+                  <FontAwesomeIcon icon={faAngleDoubleUp} /> See Less
+                </Button>
+              )}
+            </Card.Text>
+            <Button
+              variant="primary"
+              onClick={() => handleAddToFavorites(props)}
+            >
+              Add to Favorites
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <ModalMovie
+        showModal={showModal}
+        handleModalClose={handleModalClose}
+        selectedMovie={selectedMovie}
+        comment={comment}
+        setComment={setComment}
+      />
+    </>
+  );
 }
 
 export default Movie;
